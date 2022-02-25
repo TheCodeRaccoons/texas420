dd96c0d6d2590d5eae502dfae92d6a51b65b258a
Joorch <jacortez_94@hotmail.com>
2022-02-25 14:34:51 -0600

    Updated project to work with the WYSIWYG component, added react router and .env file to store environment variables

--- src/App.js
+++ src/App.js
@@ -1,54 +1,40 @@
 import React,{Component} from 'react'; 
 import './App.css';
 import './main.scss'
-import Landing from './components/Landing/Landing'
+import Home from './Containers/Home'
 import Navigation from './components/Navigation/Navigation'  
- 
-class App extends Component { 
-  constructor(props){
-    super(props);  
-    this.state = {
-      scrollTop: 0,
-      activeMenu: 'sideNav',
-      showDisc: false,
-      showCal: false
-    }
+import Wysiwyg from './components/utils/WYSIWYG/WYSIWYG';
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
 
-  }  
-  showDiscord = () => {
-    this.setState({
-      showDisc: !this.state.showDisc
-    })
+class App extends Component {   
+
+  constructor(props){
+      super(props);  
+      this.state = {
+        scrollTop: 0,
+        activeMenu: 'sideNav'
+    }
   }
-  showCalendar = () => {
-    this.setState({
-      showCal: !this.state.showCal
-    })
-  }
-  
+
   HighlightNav = (clss) => {
     if(clss !== this.state.activeMenu){ 
-      this.setState({
-        activeMenu: clss
-      })
+        this.setState({
+            activeMenu: clss
+        })
     }
   }
-  
+
   render(){ 
     return (
       <div className="App" >  
-        <Navigation navClass={this.state.activeMenu} />
-        <div className="disc-pup" onClick={this.showDiscord}></div>
-        <div className={this.state.showDisc ? "discord show" : "discord"}>
-          <span className="closeTab" onClick={this.showDiscord}>X</span> 
-          <iframe title="Disc widget" src="https://discordapp.com/widget?id=663941127757627393&theme=dark" width="350" height="500" allowtransparency="true" frameBorder="0"></iframe>
-        </div>
-        <div className="cal-pup" onClick={this.showCalendar}></div>
-        <div className={this.state.showCal ? "calendar show" : "calendar"}> 
-          <span className="closeTab" onClick={this.showCalendar} >X</span>
-          <iframe title="Google cal" src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;ctz=America%2FChicago&amp;src=YXBtNGRkaDl2cTlnazlwZjM0aGdnMWVrZThAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%233F51B5&amp;color=%230B8043"   width="350" height="500" frameBorder="0" scrolling="no"></iframe>
-        </div> 
-          <Landing  onUpdateMenu={this.HighlightNav}/>  
+      <Router>
+        <Navigation navClass={this.state.activeMenu} /> 
+        <Routes > 
+          <Route exact path="/admin_db" element={<Wysiwyg  HighlightNav={this.HighlightNav} />} />
+          <Route path="/" element={<Home HighlightNav={this.HighlightNav} />} />
+          <Route component="" />  
+        </Routes >
+      </Router>
       </div>
     );
   }
