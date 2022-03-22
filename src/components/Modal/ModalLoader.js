c24e3c85f64ee2c039e7e7dfc4da07481f7a02a2
Joorch <jacortez_94@hotmail.com>
2022-03-21 22:00:53 -0600

    adding error description handler to the update modal

--- src/components/Modal/ModalLoader.js
+++ src/components/Modal/ModalLoader.js
@@ -5,7 +5,7 @@ const ModalLoader = (props) => {
     return(
         <div className='modal-loader-container'>
             <div className='modal-body'>
-                <h2>Updating...</h2>
+                <h2>{(props.errorDesc !== "") ? props.errorDesc :"Updating..."}</h2>
                 <div className="lds-hourglass"></div>
                 {(props.updateError) ? <button className='btn-continue' onClick={props.resetModal}>Continue</button> : ""}
             </div>
